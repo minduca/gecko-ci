a48451eaed72c0875c3090b9c476f253a7228c53
Arthur Minduca <arthur.minduca@gmail.com>
2016-08-26 08:36:24 -0400

    Credits added

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -12,6 +12,17 @@ export class ObjectHelper {
 
     public static merge(defaultData, data): any {
 
+        /*
+         * CREDITS 
+         * https://gist.github.com/mazhar266/c9ba906002f1c74ba67d
+         * 
+         * @name merge
+         * @author Mazhar Ahmed
+         *
+         * this function will merge more than one json objects into one object
+         *
+         */
+         
         let destination = {},
             sources = [].slice.call(arguments, 0);
 
